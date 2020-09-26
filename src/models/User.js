9ad43a43c0e5a140d07cea4a5b9dd284715f34ab
Flavio Neiland Paz <flavio.paz@insidesistemas.com.br>
2020-09-26 18:56:26 -0300

    Feat: commit update routes

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
 
-    email: String,
+    whatsapp: String,
 });
 
 module.exports = mongoose.model('User', UserSchema );
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 const routes = require('./routes');
 
@@ -10,6 +12,7 @@ mongoose.connect('mongodb+srv://projetoBaia:projetobaia@projectbaia.zqgvx.mongod
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
